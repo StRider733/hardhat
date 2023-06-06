324fd3bfb1a4d8d231a5df8e97bb66b81c624c1f
StRider733 <fhmrider@gmail.com>
2023-06-07 02:05:44 +0300

    deployed succesfully

--- scripts/deploy-helloSolidity.js
+++ scripts/deploy-helloSolidity.js
@@ -2,14 +2,25 @@
 const hre = require ("hardhat");
 
 async function main(){
-    const greeter = hre.ethers.getContractFactory("HelloSolidity");
+    const greeter = await hre.ethers.getContractFactory ("HelloSolidity");
 
-    const deployed_greeter = await greeter.deploy("Hello, Hardhat");
+    //deploying contract
+    const deployed_greeter = await greeter.deploy ("Hello, Hardhat!");
 
+    //waiting for the contract to be deployed
     await deployed_greeter.deployed();
     
+
     console.log("Hello Solidity contract address : ", deployed_greeter.address);
 
 
 
 }
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.log("error ",error);
+        process.exit(1);
+    })
+
